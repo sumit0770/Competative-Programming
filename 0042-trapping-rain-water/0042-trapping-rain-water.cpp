5c708f7de2a4db0ce904f8bda2a7e932473f2887
Sumit Sangale <145712763+sumit0770@users.noreply.github.com>
2026-09-26 07:17:02 +0530

    Time: 0 ms (100%), Space: 25.9 MB (77.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        int n = h.size();
-
-        vector<int> pmax(n, 0);
-        vector<int> smax(n, 0);
-
-       
-        pmax[0] = h[0];
-
-        for (int i = 1; i < n; i++) {
-            pmax[i] = max(pmax[i - 1], h[i]);
-        }
-
-       
-        smax[n - 1] = h[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            smax[i] = max(smax[i + 1], h[i]);
-        }
-
+        int l= 0, r = h.size() - 1 , lmax = 0, rmax= 0;
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            ans += min(pmax[i], smax[i]) - h[i];
+        while( l < r ){
+            lmax = max ( lmax , h[l]) ;
+            rmax = max( rmax , h[r] );
+            if( lmax <= rmax){
+               ans += lmax - h[l] ;
+               l++;
+            }
+            else{
+             ans+= rmax - h[r];
+             r--;
+            }
         }
-
-        return ans;
+        return ans ;
     }
 };
